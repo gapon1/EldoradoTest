48e1355ca8b3a6449d83ab3a18a4f54346de2f36
Vitaliy <gapon007@ukr.net>
2020-08-13 23:00:42 +0300

    DB example file

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=eldorado',
+    'dsn' => 'mysql:host=localhost;dbname=eldorado_db',
     'username' => 'root',
-    'password' => 'test02156',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
